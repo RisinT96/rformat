6cc05f233d6cde27ca34a7983cf7572743f18bdb
Tal Risin <risint96@gmail.com>
2025-04-26 17:03:22 +0300

    simplify formatting detection, rename argument to formattable
    Signed-off-by: Tal Risin <risint96@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod argument;
+pub mod formattable;
 pub mod prelude;
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,5 @@
-pub use crate::argument::{
-    Argument, BinaryKind, DebugKind, DisplayKind, LowerExpKind, LowerHexKind, NoBinaryKind,
-    NoDebugKind, NoDisplayKind, NoLowerExpKind, NoLowerHexKind, NoOctalKind, NoPointerKind,
-    NoUpperExpKind, NoUpperHexKind, OctalKind, PointerKind, UpperExpKind, UpperHexKind, Wrap,
+pub use crate::formattable::{
+    Formattable, MaybeBinary, MaybeDebug, MaybeDisplay, MaybeLowerExp, MaybeLowerHex, MaybeOctal,
+    MaybePointer, MaybeUpperExp, MaybeUpperHex,
 };
-pub use crate::{argument_option, to_argument};
+pub use crate::to_formattable;
