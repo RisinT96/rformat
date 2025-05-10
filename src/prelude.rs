7c15a189be7453968d67d727ffbe237963197785
Tal Risin <risint96@gmail.com>
2025-05-10 12:36:22 +0300

    switch to trait-mux crate

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,1 @@
-pub use crate::formattable::{
-    Formattable, MaybeBinary, MaybeDebug, MaybeDisplay, MaybeLowerExp, MaybeLowerHex, MaybeOctal,
-    MaybePointer, MaybeUpperExp, MaybeUpperHex,
-};
-pub use crate::to_formattable;
+pub use crate::formattable::*;
