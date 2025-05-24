0476630ea9b583efe37d732e58dc7dc433fe68ac
Tal Risin <risint96@gmail.com>
2025-05-24 15:01:05 +0300

    fix rformat macro to take 0 parameters

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,9 +5,12 @@ pub use crate::{
 
 #[macro_export]
 macro_rules! _rformat {
-    ($format_str:expr,$($params:expr),* $(,)?) => (
-        $crate::fmt::format::format_string($format_str, &$crate::fmt::format::params![$($params),*])
-    );
+    ($fmt:expr) => {
+        $crate::fmt::format::format_string($fmt, &[])
+    };
+    ($fmt:expr, $($args:tt)*) => {
+        $crate::fmt::format::format_string($fmt, &$crate::fmt::format::params![$($args)*])
+    };
 }
 
 pub use _rformat as rformat;
